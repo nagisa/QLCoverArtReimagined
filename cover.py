3c4b368ff5a5d6f96e541bcceeb780388e9565f4
Simonas Kazlauskas <git@kazlauskas.me>
2013-09-08 13:15:57 +0300

    Another shot at cleaning up
    Should handle most of corner cases like searching for lastfm cover for song
    without MBID

--- cover.py
+++ cover.py
@@ -1,6 +1,8 @@
 from os import path
 from gi.repository import Gio, GLib, Gtk, Soup
 import json
+from functools import partial
+from hashlib import sha256
 
 from quodlibet.plugins.events import EventPlugin
 from quodlibet import app
@@ -11,11 +13,16 @@ session.set_properties(user_agent="QL Automatic Cover Fetcher Plugin")
 cover_dir = path.join(GLib.get_user_cache_dir(), 'quodlibet', 'covers')
 old_find_cover = None
 
+
 class SoupDownloaderMixin(object):
     """ This Mixin's entry point is cover_fetched as a callback from
     `session.queue_message` and it expects that data attribute is a mutable
     dictionary.
     """
+    def should_download(self, url):
+        hour_us = 3600000000
+        return GLib.get_real_time() - self.data.get(url, 0) > hour_us
+
     def cover_fetched(self, session, message, data):
         if self.cancellable and self.cancellable.is_cancelled():
             print_d('Plugin was disabled while the cover was being downloaded')
@@ -23,10 +30,14 @@ class SoupDownloaderMixin(object):
 
         if 200 <= message.get_property('status-code') < 400:
             data['body'] = message.get_property('response-body')
-            data['file'] = self.cover_gfile
+            data['file'] = Gio.file_new_for_path(self.cover_path)
             data['file'].replace_async(None, False, Gio.FileCreateFlags.NONE,
                                        GLib.PRIORITY_DEFAULT, self.cancellable,
                                        self.cover_file_opened, data)
+        elif 100 <= message.get_property('status-code'):
+            # Status codes less than 100 usually mean a failure on our side
+            # (e.g. no internet connection)
+            self.callback(False)
         else:
             self.data[self.url] = GLib.get_real_time()
             self.callback(False)
@@ -40,13 +51,13 @@ class SoupDownloaderMixin(object):
     def cover_written(self, stream, result, data):
         stream.write_bytes_finish(result)
         stream.close(None)  # Operation is cheap enough to not care about async
-        self.cover_path = data['file'].get_path()
+        self.data[self.url] = GLib.get_real_time()
         self.callback(True)
 
 
 class CoverProvider(object):
-    """ Abstracts all cover providers that store path to their fetched cover
-    in ~#cover-path
+    """
+    A base class of all cover providers.
     """
     def __init__(self, song, cancellable=None, callback=lambda x: x,
                  data=None):
@@ -57,59 +68,76 @@ class CoverProvider(object):
 
     @property
     def cover_path(self):
-        return self.song.get('~#cover-path', None)
+        """ Returns where the cover would be stored for this song, however
+            it doesn't neccessarily mean the cover is actually at the returned
+            location.
 
-    @cover_path.setter
-    def cover_path(self, val):
-        self.song['~#cover-path'] = val
+            Can return None meaning this cover provider wouldn't be able
+            to store this cover due to the lack of information.
+        """
+        return None
 
     def find_cover(self):
-        if self.cover_path and path.isfile(self.cover_path):
-            return open(self.cover_path, 'rb')
+        """ Entry method for finding the cover.
+
+            Should always return a file-like object opened as read-only if any
+            and None otherwise.
+        """
+        cp = self.cover_path
+        return open(cp, 'rb') if cp and path.isfile(cp) else None
 
     def fetch_cover(self):
+        """ Entry method for downloading the cover """
         self.callback(False)
 
 
 class MusicBrainzCoverProvider(CoverProvider, SoupDownloaderMixin):
     @property
-    def url(self):
-        return 'http://coverartarchive.org/release/{0}/front'.format(self.mbid)
-
-    @property
-    def cover_gfile(self):
-        return Gio.file_new_for_path(self.mb_cover_path)
+    def cover_path(self):
+        return path.join(cover_dir, self.mbid) if self.mbid else None
 
     @property
     def mbid(self):
         return self.song.get('musicbrainz_albumid', None)
 
     @property
-    def mb_cover_path(self):
-        return path.join(cover_dir, self.mbid)
-
-    def find_cover(self):
-        cover = super(MusicBrainzCoverProvider, self).find_cover()
-        if cover: return cover
-        elif path.isfile(self.mb_cover_path):
-            print_d('Found already existing cover art, stopping')
-            self.cover_path = self.mb_cover_path
-            return open(self.mb_cover_path, 'rb')
-        else: return None
+    def url(self):
+        if not self.mbid:
+            return None
+        return 'http://coverartarchive.org/release/{0}/front'.format(self.mbid)
 
     def fetch_cover(self):
         if not self.mbid:
             print_d("Album MBID is required to fetch the cover, stopping")
             return self.callback(False)
-        hour = 3600000000
-        if GLib.get_real_time() - self.data.get(self.url, 0) < hour:
-            print_d('Tried downloading this not too long ago, stopping')
+        if not self.should_download(self.url):
+            print_d('Decided against downloading cover from MB, stopping')
             return self.callback(False)
-        msg = Soup.Message.new('GET', self.url)
-        session.queue_message(msg, self.cover_fetched, {})
+        else:
+            msg = Soup.Message.new('GET', self.url)
+            session.queue_message(msg, self.cover_fetched, {})
 
 
 class LastFMCoverProvider(CoverProvider, SoupDownloaderMixin):
+    @property
+    def cover_path(self):
+        if self.key:
+            return path.join(cover_dir, self.key)
+
+    @property
+    def key(self):
+        mbid = self.song.get('musicbrainz_albumid', None)
+        artist = self.song.get('artist', None)
+        album = self.song.get('album', None)
+        if mbid:
+            return path.join(cover_dir, mbid)
+        elif album and artist:
+            key = sha256()
+            key.update(artist.encode('utf-8'))
+            key.update(album.encode('utf-8'))
+            return path.join(cover_dir, key.hexdigest())
+        else: return None
+
     @property
     def url(self):
         _url = 'http://ws.audioscrobbler.com/2.0?method=album.getinfo&' + \
@@ -118,42 +146,17 @@ class LastFMCoverProvider(CoverProvider, SoupDownloaderMixin):
         artist = Soup.URI.encode(self.song.get('artist', ''), None)
         album = Soup.URI.encode(self.song.get('album', ''), None)
         mbid = Soup.URI.encode(self.song.get('musicbrainz_albumid', ''), None)
-        if not (artist and album) or not mbid:
+        if artist and album or mbid:
+            return _url.format(artist=artist, album=album, mbid=mbid)
+        else:
             return None  # Not enough data
-        return _url.format(artist=artist, album=album, mbid=mbid)
-
-    @property
-    def cover_gfile(self):
-        return Gio.file_new_for_path(self.lastfm_cover_path)
-
-    @property
-    def lastfm_mbid(self):
-        return self.song.get('~#lastfm-musicbrainz-albumid', None)
-
-    @lastfm_mbid.setter
-    def lastfm_mbid(self, val):
-        self.song['~#lastfm-musicbrainz-albumid'] = val
-
-    @property
-    def lastfm_cover_path(self):
-        return path.join(cover_dir, self.lastfm_mbid)
-
-    def find_cover(self):
-        cover = super(LastFMCoverProvider, self).find_cover()
-        if cover: return cover
-        elif self.lastfm_mbid and path.isfile(self.lastfm_cover_path):
-            print_d('Found already existing cover art, stopping')
-            self.cover_path = self.lastfm_cover_path
-            return open(self.lastfm_cover_path, 'rb')
-        else: return None
 
     def fetch_cover(self):
         if not self.url:
             print_w('Not enough data to get cover from LastFM, stopping')
             return self.callback(False)
-        hour = 3600000000
-        if GLib.get_real_time() - self.data.get(self.url, 0) < hour:
-            print_d('Tried downloading this not too long ago, stopping')
+        if not self.should_download(self.url):
+            print_d('Decided against downloading cover from LastFM, stopping')
             return self.callback(False)
 
         msg = Soup.Message.new('GET', self.url)
@@ -161,17 +164,17 @@ class LastFMCoverProvider(CoverProvider, SoupDownloaderMixin):
 
     def json_fetched(self, session, message, data):
         if self.cancellable.is_cancelled():
-            print_d('Plugin was disabled while the cover was being downloaded')
+            print_d('Plugin was disabled while the album data was being' +
+                    ' downloaded')
             return self.callback(False)
 
         # LastFM always return code 200, but we might still get
-        # something else in some corner case
+        # something else in who knows what cases.
         if 200 <= message.get_property('status-code') < 400:
-            resp = message.get_property('response-body').flatten().get_data()
-            resp_json = json.loads(resp)
-            album = resp_json.get('album', {})
+            r = message.get_property('response-body').flatten().get_data()
+            rjson = json.loads(r)
+            album = rjson.get('album', {})
             if album:
-                self.lastfm_mbid = album['mbid']
                 covers = dict((img['size'], img['#text'])
                               for img in album['image'])
                 cover = covers.get('mega', covers.get('extralarge', None))
@@ -182,10 +185,15 @@ class LastFMCoverProvider(CoverProvider, SoupDownloaderMixin):
                     print_d('Could not get cover image of satisfactory size')
                     self.callback(False)
                 return
+        elif 100 <= message.get_property('status-code'):
+            # Status codes less than 100 usually mean a failure on our side
+            # (e.g. no internet connection)
+            self.callback(False)
+            return
 
+        print_w('Could not get JSON from LastFM')
         self.data[self.url] = GLib.get_real_time()
         self.callback(False)
-        print_w('Could not get JSON from LastFM')
 
 
 class AutomaticCoverFetcher(EventPlugin):
@@ -198,7 +206,7 @@ class AutomaticCoverFetcher(EventPlugin):
     cancellable = Gio.Cancellable.new()
     data = {}
     cover_providers = (MusicBrainzCoverProvider, LastFMCoverProvider,
-                       CoverProvider)
+                       CoverProvider,)
     current_song = None
 
     def enabled(self):
@@ -230,12 +238,14 @@ class AutomaticCoverFetcher(EventPlugin):
         if song.find_cover(fallback=False):
             return  # We've got nothing to do
 
-        def run(to_try, result):
+        def _run(sources, result=None):
             if result and song is self.current_song:
                 app.window.top_bar.image.set_song(song)
-            elif to_try:
-                callback = lambda x: run(to_try, x)
-                to_try.pop(0)(song, self.cancellable, callback,
-                              self.data).fetch_cover()
-
-        run(list(self.cover_providers), None)
+                return
+            try:
+                fetcher = next(sources)(song, self.cancellable, run, self.data)
+                fetcher.fetch_cover()
+            except StopIteration:
+                pass # We have no more sources.
+        run = partial(_run, reversed(self.cover_providers))
+        run()
